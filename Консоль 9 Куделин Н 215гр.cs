4469c79b30c9aa100740da2807bbdc0061a5dc0a
KudelinNikita <Rokotanskiy8@gmail.com>
2024-11-19 13:21:41 +0200

    Add files via upload

---
+++ Консоль 9 Куделин Н 215гр.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Консоль_9_Кудеилин.Н__215_гр
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ЗАДАЧА 1.Определить, имеются ли в одномерном массиве случайных чисел элементы,
+            //значения которых кратны 11.Вывести такие значения.
+
+            //int[] omas = new int[11];
+            //Console.Write("Заполните ондомерный массив");
+            //for (int i = 0; i < omas.Length;i++) 
+            //{
+            //    Console.WriteLine($"omas[{i}]=");
+            //    omas[i] = Convert.ToInt32(Console.ReadLine());
+            //}
+            //foreach (int el in omas)
+            //{
+
+            //    Console.Write(el);
+            //}
+            //Console.Read();
+
+            //ЗАДАЧА 2.Найти сумму и количество положительных элементов в одномерном массиве с явной инициализацией.
+
+            //int[] omas = new int[5]{ 1, 2, 5, 2, 3 };
+            //int count = 0, s = 0;
+            //for (int i = 0; i < omas.Length; i++) 
+            //{
+            //    s += omas[i];
+            //    count++;
+            //}
+            //Console.Write($"Сумма :{s} \n Кол-во:{count}");
+            //Console.Read();
+
+            //ЗАДАЧА 3.Вывести элементы одномерного массива, заполненного с клавиатуры, в обратном порядке.
+
+            //Console.Write("Введите количество элементов массива: ");
+            //int n = int.Parse(Console.ReadLine());
+            //int[] omas = new int[n];
+            //Console.WriteLine("Введите элементы массива:");
+            //for (int i = 0; i < n; i++)
+            //{
+            //    omas[i] =
+            //int.Parse(Console.ReadLine());
+            //}
+            //    int.Parse(Console.ReadLine());
+            //    int[] omas = new int[] { 1, 2, 3, 2, 3 };
+            //Console.WriteLine("Элементы массива 'omas' в обратном порядке:");
+            //for (int i = omas.Length - 1; i >= 0; i--)
+            //{
+            //    Console.Write(omas[i] + " ");
+            //}
+            //Console.ReadLine();
+
+        }
+    }
+}
